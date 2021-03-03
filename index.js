12095d7829fc162598355c86cdb33aa08bc3e66c
rajv79 <rajviv79@gmail.com>
2021-03-03 12:33:07 -0500

    Modified the js file and csss

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
     /********************* javascript/jquery code ***********************/
     $("h1").on("mouseover", function(){
       $("h1").css("color","#f6f7d7");
-    })
+    });
 
 
     $("h2").on("mouseover", function(){
 
           $("h2").css("color","#fc5c9c");
 
-    })
+    });
 
     $(".skill2").on("mouseover",function(){
       $(".skill2").attr("src","image/webdeveloper.gif");
 
-    })
+    });
 
     $(".project2").on("mouseover",function(){
       $(".project2").attr("src","image/programming02.gif");
 
-    })
+    });
 
     $(".project1").on("mouseover",function(){
       $(".project1").attr("src","image/coding1.gif");
 
-    })
+    });
 
     $(".skill1").on("mouseover",function(){
       $(".skill1").attr("src","image/programming01.gif");
 
+    });
+
+    $(".circular").on("mouseover",function(){
+      $(".circular").attr("src","image/boy.gif");
     })
